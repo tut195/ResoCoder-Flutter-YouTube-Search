0f7b4e890dfff0ec66cdf96f1d5883127b328bdc
vladimir <babenkovladimirbmd@gmail.com>
2019-03-04 09:12:01 +0200

    initial commit

---
+++ android/app/src/main/kotlin/com/reso_coder_youtube_search/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.reso_coder_youtube_search
+
+import android.os.Bundle
+
+import io.flutter.app.FlutterActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    GeneratedPluginRegistrant.registerWith(this)
+  }
+}
